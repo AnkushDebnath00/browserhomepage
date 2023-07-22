cb9bd099752394c83f0f2177676d9d4622e13db2
Ankush Debnath <ankushdebnath680@gmail.com>
2023-07-22 23:45:55 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
 const Header = () => {
+  let [query, setQuery] = "";
   let date = new Date();
   date = date.toDateString();
   //   let time =
   // date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
   return (
-    <>
-      <div className="font-rajdhani font-semibold text-xl">{date}</div>
+    <div className="flex flex-row justify-between">
+      <div className="font-semibold text-xl">{date}</div>
       {/* <div>{time}</div> */}
-      <div>Header</div>
-    </>
+      <form action="	https://bing.com/search">
+        <input
+          type="text"
+          id="query"
+          disabled=""
+          class=" rounded-lg border-transparent flex-1 appearance-none border w-full py-2 px-4 bg-black text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none"
+          name="q"
+          placeholder="query"
+        />
+      </form>
+    </div>
   );
 };
 
