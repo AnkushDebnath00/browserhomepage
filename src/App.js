a82ff3e7c0240f8583ed22f1e9410537fa30daf7
Ankush Debnath <ankushdebnath680@gmail.com>
2023-07-23 12:06:15 +0530

    local

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import Header from "./components/Header";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Unsplash, { toJson } from "unsplash-js";
+import {
+  faGithub,
+  faDev,
+  faAmazon,
+  faFlipboard,
+  faMakefile,
+  faYoutube,
+  faGoogleDrive,
+  faMicrosoft,
+  faGmail,
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
+  const [time, setTime] = useState("");
+  const [date, setDate] = useState("");
+  const [bgImage, setBgImage] = useState("");
+
+  let unsplash = createApi({
+    Fetch: fetch,
+  });
+
   return (
     <div className="App">
       <Header />
